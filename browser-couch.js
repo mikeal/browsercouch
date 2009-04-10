d7ba0595335b4b585e3134be2342c8a5da172aed
Atul Varma <varmaa@toolness.com>
2009-04-10 05:15:29 -0700

    We now use DOM storage to persistently store the DB, and dynamically load a JSON library when necessary to serialize/deserialize data.

--- browser-couch.js
+++ browser-couch.js
@@ -1,20 +1,81 @@
+
 var BrowserCouch = {
-  get: function BC_get(name, cb) {
-    cb(new this._DB(name, []));
-  },
+  get: function BC_get(name, cb, storage, JSON) {
+    var self = this;
 
-  _DB: function BC__DB(name, documents) {
-    var docIdIndex = {};
-
-    function putSingleDocument(doc) {
-      if (doc.id in docIdIndex)
-        documents[docIdIndex[doc.id]] = doc;
-      else
-        docIdIndex[doc.id] = documents.push(doc);
+    function createDb() {
+      cb(new self._DB(name, storage, JSON));
     }
 
-    for (var i = 0; i < documents.length; i++)
-      putSingleDocument(documents[i]);
+    if (!storage) {
+      if (window.globalStorage || window.localStorage) {
+        if (window.globalStorage)
+          storage = window.globalStorage[location.hostname];
+        else
+          storage = window.localStorage;
+        if (window.JSON) {
+          JSON = window.JSON;
+          createDb();
+        } else
+          self._loadScript(
+            "json2.js",
+            window,
+            function() {
+              if (!window.JSON)
+                throw new Error('JSON library failed to load');
+              JSON = window.JSON;
+              createDb();
+            });
+      } else {
+        /* TODO: Consider using JSPersist or something else here. */
+        throw new Error('unable to find persistent storage backend');
+      }
+    } else
+      createDb();
+  },
+
+  _loadScript: function BC__loadScript(url, window, cb) {
+    var doc = window.document;
+    var script = doc.createElement("script");
+    script.setAttribute("src", url);
+    script.addEventListener(
+      "load",
+      function onLoad() {
+        script.removeEventListener("load", onLoad, false);
+        cb();
+      },
+      false
+    );
+    doc.body.appendChild(script);
+  },
+
+  _DB: function BC__DB(name, storage, JSON) {
+    var dbName = 'BrowserCouch_DB_' + name;
+
+    var documents = [];
+    var docIdIndex = {};
+
+    if (dbName in storage && storage[dbName].value) {
+      console.log(storage[dbName].value);
+      var db = JSON.parse(storage[dbName].value);
+      documents = db.documents;
+      docIdIndex = db.docIdIndex;
+    }
+
+    function commitToStorage() {
+      storage[dbName].value = JSON.stringify(
+        {documents: documents,
+         docIdIndex: docIdIndex}
+      );
+    }
+
+    this.wipe = function DB_wipe(cb) {
+      documents = [];
+      docIdIndex = {};
+      commitToStorage();
+      if (cb)
+        cb();
+    };
 
     this.get = function DB_get(id, cb) {
       if (id in docIdIndex)
@@ -24,11 +85,19 @@ var BrowserCouch = {
     };
 
     this.put = function DB_put(document, cb) {
+      function putSingleDocument(doc) {
+        if (doc.id in docIdIndex)
+          documents[docIdIndex[doc.id]] = doc;
+        else
+          docIdIndex[doc.id] = documents.push(doc) - 1;
+      }
+
       if (document.constructor.name == "Array") {
         for (var i = 0; i < document.length; i++)
           putSingleDocument(document[i]);
       } else
         putSingleDocument(document);
+      commitToStorage();
       cb();
     };
 
@@ -77,9 +146,12 @@ var BrowserCouch = {
   }
 };
 
-var gCouch = BrowserCouch.get(
+var gDb;
+
+BrowserCouch.get(
   "blarg",
   function(db) {
+    gDb = db;
     console.log(db);
     db.put(
       [{id: "monkey",
