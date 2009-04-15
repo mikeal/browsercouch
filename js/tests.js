bafcc7b02a5302179ff9f37a7183c527d78b04cc
Atul Varma <varmaa@toolness.com>
2009-04-15 09:27:19 -0700

    Added docs for tests.js, and refactored it a bit to make the docs easier to read.

--- js/tests.js
+++ js/tests.js
@@ -1,7 +1,55 @@
-var Tests = {
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Ubiquity.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Atul Varma <atul@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+// = BrowserCouch Test Suite =
+//
+// This file contains a simple test runner and suite for BrowserCouch.
+
+// == The Runner ==
+//
+// The {{{Testing}}} namespace is a simple test framework that supports
+// the skipping of tests (for when the host system doesn't support
+// required functionality for a test's execution), as well as
+// asynchronous tests.
+//
+// It doesn't currently have the ability to detect when an asynchronous
+// test has failed, however.
+
+var Testing = {
   run: function(listener, container, setTimeout) {
-    if (!container)
-      container = this;
     if (!setTimeout)
       setTimeout = window.setTimeout;
 
@@ -45,7 +93,15 @@ var Tests = {
     }
 
     runNextTest();
-  },
+  }
+};
+
+// == The Suite ==
+//
+// The {{{Tests}}} namespace contains the actual testing suite for
+// BrowserCouch.
+
+var Tests = {
   testDictionary: function(self) {
     var dict = new BrowserCouch._Dictionary();
     dict.set('foo', {a: 'hello'});
